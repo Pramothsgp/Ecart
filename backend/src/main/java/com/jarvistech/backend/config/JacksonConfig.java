871aeb408d2460057810a250dbd6c5f7564e4555
Pramothsgp <pramothsgp@gmail.com>
2025-01-13 20:37:46 +0530

    Json Nesting depth Incresed

---
+++ backend/src/main/java/com/jarvistech/backend/config/JacksonConfig.java
@@ -0,0 +1,23 @@
+package com.jarvistech.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.core.StreamWriteConstraints;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.getFactory().setStreamWriteConstraints(
+            StreamWriteConstraints.builder()
+                .maxNestingDepth(2000)
+                .build()
+        );
+        return mapper;
+    }
+
+}

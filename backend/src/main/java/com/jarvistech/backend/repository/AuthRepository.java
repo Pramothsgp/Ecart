1fa47e60fe704da66932a026b15966b3239ed867
Pramothsgp <pramothsgp@gmail.com>
2025-02-20 15:44:55 +0530

    Security Configuration Updated

--- backend/src/main/java/com/jarvistech/backend/repository/AuthRepository.java
+++ backend/src/main/java/com/jarvistech/backend/repository/AuthRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.jarvistech.backend.model.User;
 
 public interface AuthRepository extends JpaRepository<User, Long> {
-    
+
     Optional<User> findUserByUsernameOrEmail(String username, String email);
 }

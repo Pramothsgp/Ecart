c7cca4c46217e7ceab3519bd34ef738d784ce6b8
Pramothsgp <pramothsgp@gmail.com>
2025-03-26 07:34:09 +0530

    swagger search products and pagination for products

--- backend/src/main/java/com/jarvistech/backend/service/ProductService.java
+++ backend/src/main/java/com/jarvistech/backend/service/ProductService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.jarvistech.backend.dto.CommentRequestDTO;
@@ -64,5 +66,16 @@ public class ProductService {
         comment1.setComment(comment.getComment());
         comment1.setComment(comment.getComment());
         return commentRepository.save(comment1);
+    }
+
+
+    public List<Product> getPages(int pageNumber , int pageSize){
+        Pageable page = PageRequest.of(pageNumber, pageSize);
+        return productRepository.findAll(page).getContent();
+    }
+
+
+    public List<Product> searchProducts(String key) {
+        return productRepository.findByProductNameContainingIgnoreCase(key);
     }
 }
